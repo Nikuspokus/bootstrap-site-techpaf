dc346e969b0a9490d7570703a92fdbeeee4da63a
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-05-07 14:12:53 +0200

    Styling project techpat - integration du carousel - gestion des images - WIP

---
+++ src/components/Carousel-0.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <b-carousel
+      id="carousel-fade"
+      style="text-shadow: 0px 0px 2px #000"
+      fade
+      indicators
+      img-width="100vh !important"
+    >
+      <b-carousel-slide
+        class="carousel-item-2"
+        caption="First slide"
+
+      ></b-carousel-slide>
+      <b-carousel-slide
+        class="carousel-item-2"
+        caption="Second Slide"
+        img-src="https://picsum.photos/1024/480/?image=12"
+      ></b-carousel-slide>
+      <b-carousel-slide
+        class="carousel-item-2"
+        caption="Third Slide"
+        img-src="https://picsum.photos/1024/480/?image=22"
+      ></b-carousel-slide>
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Carousel",
+};
+</script>
+
+<style>
+.carousel-item img {
+  width: 100vh !important ;
+}
+.carousel-item-2 img {
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,35 +1,71 @@
 <template>
   <div>
     <b-carousel
-      id="carousel-fade"
-      style="text-shadow: 0px 0px 2px #000"
-      fade
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
       indicators
-      img-width="300"
-      img-height="300"
+      background="#ababab"
+      img-width="1024"
+      img-height="80"
+      style="text-shadow: 1px 1px 2px #333"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
     >
-      <b-carousel-slide
-        caption="First slide"
-        img-src="https://picsum.photos/1024/480/?image=10"
-        style="width:50%"
-      ></b-carousel-slide>
-      <b-carousel-slide
-        caption="Second Slide"
-        img-src="https://picsum.photos/1024/480/?image=12"
-        class="mw-100"
-        style="height:20%"
-      ></b-carousel-slide>
-      <b-carousel-slide
-        caption="Third Slide"
-        img-src="https://picsum.photos/1024/480/?image=22"
-        style="width:50%"
-      ></b-carousel-slide>
+      <!-- Text slides with image -->
+      
+
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+     
+
+      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+      <b-carousel-slide caption=" Image"  img-alt="Blank image" img-src="../assets/nasa-earth.jpg">src\assets\nasa-earth.jpg
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          eros felis, tincidunt a tincidunt eget, convallis vel est. Ut
+          pellentesque ut lacus vel interdum.
+        </p>
+      </b-carousel-slide>
+      <!-- <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          eros felis, tincidunt a tincidunt eget, convallis vel est. Ut
+          pellentesque ut lacus vel interdum.
+        </p>
+      </b-carousel-slide>
+      <b-carousel-slide caption="Blank " img-blank img-alt="Blank image">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          eros felis, tincidunt a tincidunt eget, convallis vel est. Ut
+          pellentesque ut lacus vel interdum.
+        </p>
+      </b-carousel-slide> -->
     </b-carousel>
+
+    <!-- <p class="mt-4">
+      Slide #: {{ slide }}<br />
+      Sliding: {{ sliding }}
+    </p> -->
   </div>
 </template>
 
 <script>
 export default {
-  name: "Carousel",
+  data() {
+    return {
+      slide: 0,
+      sliding: null,
+    };
+  },
+//   methods: {
+//     onSlideStart(slide) {
+//       this.sliding = true;
+//     },
+//     onSlideEnd(slide) {
+//       this.sliding = false;
+//     },
+//   },
 };
 </script>
